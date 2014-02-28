a29da506b660c700a380d514ac1d323aa2a8a50d
poulet_a <poulet_a@epitech.eu>
2014-02-28 16:25:15 +0100

    Add option ninit

--- lib/sloubi.rb
+++ lib/sloubi.rb
@@ -2,9 +2,10 @@
 
 module Sloubi
   class Sloubi
-    def self.count_sloubi n=42, v=false
-      return false if !n.is_a?Integer or n < 1
-      (1..n).each do |i|
+    def self.count_sloubi n=42, ninit=1, v=false
+      return 0 if !n.is_a?Integer or n < 1
+      return 0 if ninit > n
+      (ninit..n).each do |i|
         puts "Sloubi #{i}"
         `espeak "Sloubi #{i}" -a 200 -vfr` if v == true
       end
