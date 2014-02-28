ec679a005750d694e695f87b8beba5177875fa39
poulet_a <poulet_a@epitech.eu>
2014-02-28 16:15:55 +0100

    Sloubi with verbose

--- lib/sloubi.rb
+++ lib/sloubi.rb
@@ -2,10 +2,11 @@
 
 module Sloubi
   class Sloubi
-    def self.count_sloubi n=42
+    def self.count_sloubi n=42, v=false
       return false if !n.is_a?Integer or n < 1
       (1..n).each do |i|
         puts "Sloubi #{i}"
+        `espeak "Sloubi #{i}" -a 200 -vfr` if v == true
       end
     end
   end
